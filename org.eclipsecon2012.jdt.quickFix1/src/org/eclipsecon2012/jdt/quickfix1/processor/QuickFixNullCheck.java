17bb58c6e644662d55b5827cebf727935d1be5ac
Ayushman Jain <ajain>
2012-03-21 16:50:48 +0530

    tutorial2 project

---
+++ org.eclipsecon2012.jdt.quickFix1/src/org/eclipsecon2012/jdt/quickfix1/processor/QuickFixNullCheck.java
@@ -0,0 +1,81 @@
+package org.eclipsecon2012.jdt.quickfix1.processor;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.compiler.IProblem;
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
+import org.eclipse.jdt.ui.text.java.IInvocationContext;
+import org.eclipse.jdt.ui.text.java.IJavaCompletionProposal;
+import org.eclipse.jdt.ui.text.java.IProblemLocation;
+import org.eclipse.jdt.ui.text.java.IQuickFixProcessor;
+import org.eclipse.jdt.ui.text.java.correction.ASTRewriteCorrectionProposal;
+
+public class QuickFixNullCheck implements IQuickFixProcessor {
+
+	@Override
+	public boolean hasCorrections(ICompilationUnit unit, int problemId) {
+		return IProblem.PotentialNullLocalVariableReference == problemId;
+	}
+
+	@Override
+	public IJavaCompletionProposal[] getCorrections(IInvocationContext context,
+			IProblemLocation[] locations) throws CoreException {
+		HashSet<Integer> handledProblems= new HashSet<Integer>(locations.length);
+		ArrayList<IJavaCompletionProposal> resultingCollections= new ArrayList<IJavaCompletionProposal>();
+		for (int i= 0; i < locations.length; i++) {
+			IProblemLocation curr= locations[i];
+			Integer id= new Integer(curr.getProblemId());
+			if (handledProblems.add(id)) {
+				process(context, curr, resultingCollections);
+			}
+		}
+		return resultingCollections.toArray(new IJavaCompletionProposal[resultingCollections.size()]);
+	}
+	
+	private void process(final IInvocationContext context,
+			final IProblemLocation problem,
+			final ArrayList<IJavaCompletionProposal> proposals)
+			throws CoreException {
+		final int id = problem.getProblemId();
+		if (id == 0) { // no proposals for none-problem locations
+			return;
+		}
+		switch (id) {
+			// 1. Which proposal(s) do we want. Call a method to handle each proposal.
+		}
+	}
+	
+	public static void addNullCheckProposal(IInvocationContext context, IProblemLocation problem, Collection<IJavaCompletionProposal> proposals){
+		ICompilationUnit cu= context.getCompilationUnit();
+		ASTNode selectedNode= problem.getCoveredNode(context.getASTRoot());
+		AST ast = selectedNode.getAST();
+		
+		ASTRewrite rewrite = ASTRewrite.create(ast);
+		String label= "add null check";
+		
+		// 2. add sanity checks and AST modifications
+		
+		// 2a. What new AST nodes do we want? What AST node do we want to remove?
+		
+		// 2b. Create skeletal ifStatement, a Block to be added as the 'then' statement and an expression to be added
+		// as the condition for the ifStatement. Set the if's condition and then statement
+		
+		// 2c. Set the proper operands and operator of the if's condition expression
+		
+		// 2d. Add the earlier dereferencing statement into the if's then block
+		
+		// 2e. replace the dereferencing statement with the if statement using the 'rewrite' object.
+		
+		
+		ASTRewriteCorrectionProposal proposal= new ASTRewriteCorrectionProposal(label, cu, rewrite, 6);
+			
+		proposals.add(proposal);
+	}
+
+}
